7f7fb621461931fe6d2d94919601ed3e250eb7a9
Abdullah <abdullah-rafiq2011@live.co.uk>
2019-06-27 21:11:32 +0500

    Flat Class Added

--- Model/Building.cs
+++ Model/Building.cs
@@ -13,14 +13,19 @@ namespace DBproject.Model
         int flatsPerFloor;
         String code;
         User admin;
+        int flatNumberScheme;
+        List<Flat> flats;
         
-        public Building(string appartmentName, int noOfFloors, int flatsPerFloor, string code, User admin)
+        public Building(string appartmentName, int noOfFloors, int flatsPerFloor, string code, User admin, int flatNumberScheme)
         {
             this.appartmentName = appartmentName;
             this.noOfFloors = noOfFloors;
             this.flatsPerFloor = flatsPerFloor;
             this.code = code;
             this.admin = admin;
+            this.flatNumberScheme = flatNumberScheme;
+
+            // Create Flats Here
         }
        
     }
---
+++ Model/Flat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBproject.Model
+{
+    class Flat
+    {
+        int flatNumber;
+        string nameOfResident;
+        string email;
+        string contactNumber;
+        int dues;
+        int monthlyFees;
+        User member;
+             
+
+    }
+}
