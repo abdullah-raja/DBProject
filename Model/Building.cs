f53cb92e9c9c50c2e757dbe45ce9c9a6504b977e
Abdullah <abdullah-rafiq2011@live.co.uk>
2019-06-30 20:42:17 +0500

    Flats Added in Buildings

--- Model/Building.cs
+++ Model/Building.cs
@@ -14,7 +14,7 @@ namespace DBproject.Model
         String code;
         User admin;
         int flatNumberScheme;
-        List<Flat> flats;
+        Flat[,] flats;
         
         public Building(string appartmentName, int noOfFloors, int flatsPerFloor, string code, User admin, int flatNumberScheme)
         {
@@ -24,8 +24,20 @@ namespace DBproject.Model
             this.code = code;
             this.admin = admin;
             this.flatNumberScheme = flatNumberScheme;
-
+            this.flats = new Flat[noOfFloors, flatsPerFloor];
             // Create Flats Here
+
+            for(int i = 0; i < noOfFloors; i++)
+            {
+                for (int j = 0; j < flatsPerFloor; j++)
+                {
+                    if (flatNumberScheme == 0)
+                    {
+                        this.flats[i, j] = new Flat(flatNumberScheme, i, j);
+                        
+                    }
+                }
+            }
         }
        
     }
--- Model/Flat.cs
+++ Model/Flat.cs
@@ -9,12 +9,23 @@ namespace DBproject.Model
     class Flat
     {
         int flatNumber;
+        int floor;
         string nameOfResident;
         string email;
         string contactNumber;
         int dues;
         int monthlyFees;
         User member;
+
+        public Flat(int flatNumberScheme, int floor, int index)
+        {
+            this.floor = floor;
+            if(flatNumberScheme == 0)
+            {
+                int f = (floor * 100) + index;
+            }
+            
+        }
              
 
     }
