676481a957abfcf8e9fedbc35ccccda8d0196dca
Abdullah <abdullah-rafiq2011@live.co.uk>
2019-07-01 01:23:53 +0500

    Getters Setters Added in User Class

--- Model/User.cs
+++ Model/User.cs
@@ -8,21 +8,85 @@ namespace DBproject.Model
 {
     class User
     {
-        int ID;
+        string userID;
         String userName;
         String email;
         String password;
         bool isAdmin;
+        static int userCount = 0;
         
 
         public User(string userName, string email, string password, bool isAdmin)
         {
             this.userName = userName;
             this.email = email;
-            this.password = password;
+            this.password = encodePassword(password);
             this.isAdmin = isAdmin;
-
+            userCount++;
+            this.userID = "U" + userCount + DateTime.Today.Year.ToString(); // U232019
             
         }
+
+
+        // ID getter Setters
+        public string getID()
+        {
+            return this.userID;
+        }
+
+
+
+        // UserName Getter Setters
+        public string getUserName()
+        {
+            return this.userName;
+        }
+
+        public void setUserName(string userName)
+        {
+            this.userName = userName;
+        }
+
+
+        // Email
+
+        public string getEmail()
+        {
+            return this.email;
+        }
+
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+
+        // Password Getters Setters
+
+        public string getPassword()
+        {
+            return this.password;
+        }
+
+        // Password Encoding Decoding
+
+        public string encodePassword(string s)
+        {
+            string encodedString = "";
+            for (int i = s.Length; i >= 0; i--)
+            {
+                encodedString += s;
+            }
+
+            return encodedString;
+                
+        }
+
+        // Password Matching
+
+        public bool checkPassowrd(string p)
+        {
+            return encodePassword(p) == this.password;
+        }
+
     }
 }
