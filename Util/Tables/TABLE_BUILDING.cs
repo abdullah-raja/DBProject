165125a0e1b49d65d32cb18230a838b0d13e460f
Sarim Khan <sarimzai705@gmail.com>
2019-07-14 00:39:39 +0500

    f

---
+++ Util/Tables/TABLE_BUILDING.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBproject.Util.Tables
+{
+    class TABLE_BUILDING
+    {
+        public static string TBL_BUILDING = "tbl_Building";
+        public static string KEY_APPARTMENT_ID = "appartmentID";
+        public static string KEY_APPARTMENT_NAME = "appartmentName";
+        public static string KEY_NUMBER_OF_FLOOR = "numberOfFloors";
+        public static string KEY_FLATS_PER_FLOOR = "flatsPerFloor";
+        public static string KEY_CODE = "code";
+        public static string KEY_FLAT_NUMBER_SCHEME = "flatNumberScheme";
+        public static string KEY_ADMIN_ID = "adminID";
+        public static string KEY_BALANCE = "balance";
+    }
+}
---
+++ Util/Tables/TABLE_FLATS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBproject.Util.Tables
+{
+    class TABLE_FLATS
+    {
+        public static string TBL_FLATS = "tbl_Flats";
+        public static string KEY_FLAT_NUMBER = "flatNumber";
+        public static string KEY_FLOOR = "floor";
+        public static string KEY_USER_ID = "userId";
+        public static string KEY_EMAIL = "email";
+        public static string KEY_MOBILE_NUMBER = "mobileNumber";
+        public static string KEY_DUES = "dues";
+        public static string KEY_MONTHLYFEE = "monthlyFee";
+        public static string KEY_APPARTMENT_ID = "appartmentID";
+        public static string KEY_RESIDENT_NAME = "residentName";
+        public static string KEY_IS_MANAGER = "isManager";
+    }
+}
